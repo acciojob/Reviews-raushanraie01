baa5ada175366ce3259b92b6d1bf45dcef7bdc46
raushanraie01 <raushanraie01@gmail.com>
2025-12-11 13:08:05 +0530

    done 2

--- src/components/Review.js
+++ src/components/Review.js
@@ -18,7 +18,7 @@ function Review() {
     <div>
       <h1 id="review-heading">Our Reviews</h1>
       <div className="review" key={review.id}>
-        <p className="author">{review.name}</p>
+        <p className={`author-${review.id}`}>{review.name}</p>
         <p className="job">{review.job}</p>
         <p className="info">{review.text}</p>
         <img
